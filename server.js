aa3bbde2aec3f62c1d323ab271983fb4b6eea4e3
Anna Sychikova <asychikova@myseneca.ca>
2023-01-29 17:54:15 -0500

    initial commit

--- server.js
+++ server.js
@@ -11,37 +11,10 @@ blogService.initialize()
         });
     })
     .catch((err) => {
-        console.log(err);
+        console.log("Can't load blog."+ err);
     });
-
-/*const blogService = require('./blog-service.js');
-
-app.get('/blog', (req, res) => {
-    blogService.getAllPosts()
-      .then((posts) => {
-        res.json(posts);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).send(err);
-      });
-  }); */
-
-/*blogService.initialize()
-  .then(() => {
-    app.listen(process.env.PORT || 8080, () => {
-      console.log("Express http server listening on port", 8080);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  }); */
 
 app.use(express.static("public"));
-
-/*app.listen(process.env.PORT || 8080, () => {
-  console.log("Express http server listening on port", 8080);
-});*/
 
 app.get("/", (req, res) => {
     res.redirect("/about");
@@ -52,35 +25,36 @@ app.get("/about", (req, res) => {
  res.sendFile(__dirname + "/views/about.html");
 });
 
-/*app.get("/blog", (req, res) => {
-    //TODO: Retrieve all posts from the posts.json file where published property is true
-    res.send("TODO: get all posts");
-});
-app.get("/blog", (req, res) => {
-    blogService.getPublishedPosts()
-        .then(posts => res.send(posts))
-        .catch(err => console.log(err));
-});*/
 app.get('/blog', (req, res) => {
     blogService.getAllPosts()
         .then((posts) => {
             res.json(posts);
         })
         .catch((err) => {
-            res.json({error: err});
+          console.log("Can't load all posts." + err);
         });
 });
 
 app.get("/posts", (req, res) => {
-    //TODO: Retrieve all posts from the posts.json file
-    res.send("TODO: get all posts");
+  blogService.getPublishedPosts()
+  .then((PublishedPosts) => {
+      res.json(PublishedPosts);
+  })
+  .catch((err) => {
+      res.json({error: err});
+  });
 });
 
 app.get("/categories", (req, res) => {
-    //TODO: Retrieve all categories from the categories.json file
-    res.send("TODO: get all categories");
+  blogService.getCategories()
+  .then((categories) => {
+      res.json(categories);
+  })
+  .catch((err) => {
+      console.log("Can't load categories." + err);
+  });
 });
 
 app.get("*", (req, res) => {
-    res.status(404).send("Page Not Found 404!!!"); //how to make a design
+    res.status(404).send("Page Not Found 404!!!"); //design
 });
