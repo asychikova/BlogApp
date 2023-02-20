5f643890dcc8eaa7282a0e6ed5bcf6b9cde65aa4
Anna Sychikova <asychikova@myseneca.ca>
2023-02-20 06:55:29 -0500

    commit changes

--- blog-service.js
+++ blog-service.js
@@ -1,9 +1,10 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  WEB322 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Anna Sychikova Student ID: 159363217 Date: 30 of Jan (starded 25/26)
+*  Name: Anna Sychikova Student ID: 159363217 Date: ~12 of Feb (or earlier(when assignment was uploaded))
 *
 *  Cyclic Web App URL: https://outrageous-boa-gown.cyclic.app/about
 *
@@ -17,13 +18,13 @@ let categories = [];
 
 function initialize () {
     return new Promise((resolve, reject) => {
-        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+        fs.readFile('./data/categories.json', 'utf8', (err, data) => {
             if (err) {reject("unable to read file");}
-             else { posts = JSON.parse(data);
-                fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+             else { categories = JSON.parse(data);
+                fs.readFile('./data/posts.json', 'utf8', (err, data) => {
                     if (err) {reject("unable to read file");}
                      else {
-                        categories=JSON.parse(data);
+                        posts=JSON.parse(data);
                         resolve();}
                     });
                 }
@@ -34,16 +35,38 @@ function initialize () {
 function getAllPosts() {
     return new Promise((resolve, reject) => {
         if (posts.length==0) {
-            reject("no results returned");
+            reject("No results returned");
         } else {resolve(posts);}
     });
 }
+
+function addPost (postData) {
+  return new Promise((resolve, reject) => {
+    let post = {
+      id: posts.length + 1,
+      body: postData.body,
+      title: postData.title,
+      postDate: new Date().toISOString(),
+      category: parseInt(postData.category),
+      featureImage: postData.featureImage,
+      published: Boolean(postData.published),
+    };
+    posts.push(post);
+    fs.writeFile("data/posts.json", JSON.stringify(posts), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(post);
+      }
+    });
+  });
+};
 
 function getPublishedPosts() {
     return new Promise((resolve, reject) => {
     const publishedPosts=posts.filter(post => post.published==true);
         if (publishedPosts.length==0) {
-            reject("no results returned");
+            reject("No results returned");
         } else {resolve(publishedPosts);}
     });
 }
@@ -51,11 +74,68 @@ function getPublishedPosts() {
 function getCategories () {
     return new Promise((resolve, reject) => {
         if (categories.length==0) {
-            reject("no results returned");
+            reject("No results returned");
         } else {resolve(categories);}
     });
 }
 
-module.exports = {initialize,getAllPosts,getPublishedPosts,getCategories} 
+async function getPostsByCategory(category) {
+  try {
+    const posts = await getAllPosts();
+    const filteredPosts = posts.filter((post) => post.category === category);
+  
+    if (filteredPosts.length === 0) {
+      throw "No results returned(Category)";
+    } else {
+      // sort in ascending order by id 
+     const sortedPosts = filteredPosts.sort((a, b) => a.id - b.id);
+      return sortedPosts;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getPostsByMinDate(minDate) {
+  try {
+    const posts = await getAllPosts();
+    const filteredPosts = posts.filter(
+      (post) => new Date(post.postDate).toISOString() >= new Date(minDate).toISOString()
+    );
+    if (filteredPosts.length === 0) {
+      throw "No results returned(minDate)";
+    } else {
+      // sort in ascending order by date
+      const sortedPosts = filteredPosts.sort((a, b) => new Date(a.postDate) - new Date(b.postDate));
+      return sortedPosts;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function getPostById(id) {
+  return new Promise((resolve, reject) => {
+    const post = posts.find((post) => post.id === id);
+    if (post) {
+      resolve(post);
+    } else {
+      reject(`Post not found`); 
+    }
+  });
+}
+  
+module.exports = {
+  initialize,
+  getAllPosts,
+  getPublishedPosts,
+  getCategories,
+  addPost,
+  getPostsByCategory,
+  getPostsByMinDate,
+  getPostById
+} 
+
+
 
 
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  WEB322 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Anna Sychikova Student ID: 159363217 Date: 30 of Jan (starded 25/26)
+*  Name: Anna Sychikova Student ID: 159363217 Date: ~12 of Feb (or earlier(when assignment was uploaded))
 *
 *  Cyclic Web App URL: https://outrageous-boa-gown.cyclic.app/about
 *
@@ -13,50 +14,155 @@
 
 const express = require("express");
 const app = express();
+const path = require("path");
+
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2
+const streamifier = require("streamifier")
+const blogService = require("./blog-service");
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+const upload = multer();
 
 app.use(express.static("public"));
-var HTTP_PORT = process.env.PORT || 8080;
-const blogService = require('./blog-service');
 
 app.get("/", (req, res) => {
-    res.redirect("/about");
-   });
+  res.redirect("/about");
+ });
 
 app.get("/about", (req, res) => {
- res.sendFile(__dirname + "/views/about.html");
+res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get('/blog', (req, res) => {
+blogService.getPublishedPosts()
+.then((posts) => {
+  res.send(posts);
+})
+.catch((err) => {console.log("Can't load all posts." + err);});
+});
+
+app.get("/posts", (req, res) => {
+  let category = req.query.category;
+  let minDate = req.query.minDate;
+
+  if (category) {
+    category = parseInt(category);
+    blogService.getPostsByCategory(category)
+    .then((filteredPosts) => {
+      res.send(filteredPosts);
+      })
+      .catch((err) => {
+        res.send({ message: err });
+      });
+  } else if (minDate) {
+    blogService.getPostsByMinDate(minDate)
+    .then((filteredPosts) => {
+      res.send(filteredPosts);
+      })
+      .catch((err) => {
+        res.send({ message: err });
+      });
+  } else {
+    blogService.getAllPosts()
+      .then((posts) => {
+        res.send(posts);
+      })
+      .catch((err) => {
+        res.send({ message: err });
+      });
+  }
+});
+
+
+app.get("/post/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("ID:", id);
+  blogService
+    .getPostById(id)
+    .then((post) => {
+      console.log("Post:", post);
+      res.send(post);
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+      res.status(404).send({ message: `Post with ID ${id} not found` });
+    });
+});
+
+
+
+app.get("/categories", (req, res) => {
+blogService.getCategories()
+.then((categories) => {
+  res.send(categories);
+})
+.catch((err) => {console.log("Can't load categories. " + err);});
 });
 
 app.get("/posts/add", (req, res) => {
   res.sendFile(__dirname + "/views/addPost.html");
- });
+  });
 
-app.get('/blog', (req, res) => {
-  blogService.getAllPosts()
-  .then((posts) => {
-    res.send(posts);
-  })
-  .catch((err) => {console.log("Can't load all posts." + err);});
-});
 
-app.get("/posts", (req, res) => {
-  blogService.getPublishedPosts()
-  .then((PublishedPosts) => {
-    res.send(PublishedPosts);
-  })
-  .catch((err) => {console.log("Can't load published posts. " + err);});
-});
+  
+app.post("/posts/add", upload.single("featureImage"), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+  
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+  
+      upload(req).then((uploaded) => {
+        processPost(uploaded.url);
+      });
+    } else {
+      processPost("");
+    }
 
-app.get("/categories", (req, res) => {
-  blogService.getCategories()
-  .then((categories) => {
-    res.send(categories);
-  })
-  .catch((err) => {console.log("Can't load categories. " + err);});
-});
+      function processPost(imageUrl) {
+        req.body.featureImage = imageUrl;
+        blogService
+          .addPost(req.body)
+          .then(() => {
+            return blogService.getPublishedPosts();
+          })
+          .then((publishedPosts) => {
+            res.redirect('/posts');
+          })
+          .catch((err) => {
+            res.send({ message: err });
+          });
+        }
+  });
+  
+  app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+  });
+  
+  cloudinary.config({
+    cloud_name: 'dixlrbgil',
+    api_key: '264933176431443',
+    api_secret: 'AHNrJUccXxIK6k7Dvx-siA-AGuk',
+    secure: true
+  });
 
-app.get("*", (req, res) => {res.sendFile(__dirname + "/views/errorpage.html");});
-
-blogService
+  blogService
   .initialize()
   .then(() => {
     app.listen(HTTP_PORT, () => {
@@ -64,3 +170,6 @@ blogService
     });
   })
   .catch((err) => {console.log("Can't load blog."+ err);});
+  app.get("*", (req, res) => {res.sendFile(__dirname + "/views/errorpage.html");});
+
+  
